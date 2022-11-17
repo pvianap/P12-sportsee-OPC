355500778ab22182398e66de3c908ec80b0b8a93
pvianap <pedro.v.parente@gmail.com>
2022-11-17 16:24:33 +0100

    Folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">SportSeeApp!</header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
