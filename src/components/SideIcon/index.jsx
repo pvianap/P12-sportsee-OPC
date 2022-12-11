208ec017e1a24abba197a34377952fa7bd3a5616
pvianap <pedro.v.parente@gmail.com>
2022-12-11 09:58:13 +0100

    Barside icons corection

--- src/components/SideIcon/index.jsx
+++
@@ -1,9 +0,0 @@
-import './_sideicon.scss';
-
-/**
- * Side icons
- * @component compose icons for sidebar
- */
-export default function SideIcon({ image }) {
-  return <img src={`/sidebar_icon_${image}.png`} className="sideicon" alt="" />;
-}
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,9 @@
 import './_sidebar.scss';
-import SideIcon from '../SideIcon';
 
+import YogaIcon from '../../assets/sidebar_icon_01.png';
+import SwimmingIcon from '../../assets/sidebar_icon_02.png';
+import CyclingIcon from '../../assets/sidebar_icon_03.png';
+import GymIcon from '../../assets/sidebar_icon_04.png';
 /**
  * Sidebar of app
  * @component lateral bar with links for diferent activities
@@ -10,10 +13,10 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__container">
-        <SideIcon image="01" />
-        <SideIcon image="02" />
-        <SideIcon image="03" />
-        <SideIcon image="04" />
+        <img src={YogaIcon} alt="Icon of yoga" />
+        <img src={SwimmingIcon} alt="Icon of swimming" />
+        <img src={CyclingIcon} alt="Icon of cycling" />
+        <img src={GymIcon} alt="Icon of gym" />
       </div>
     </div>
   );
